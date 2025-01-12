3d3f75e1f5ee84bd3660a2f2e5c075767d3f45fb
Denen <awardenen@gmail.com>
2025-01-12 12:05:54 +0100

    Styled the landing page

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
+const path = require("path");
 const authController = require("../controllers/authController")
 const { authenticateCookie } = require("../middleware/authMiddleware");
 
 router.get("/", (req, res) => {
-    res.send("Welcome to the chat home page!!")
+    res.sendFile(path.join(__dirname, "../views/index.html"))
 })
 
 router.get("/login", authController.getLoginForm);
